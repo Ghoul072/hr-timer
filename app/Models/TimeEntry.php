440318419dd5d54567792a7bbc7994c7e61b9216
Yaseen <ahmed.yaseen@avasride.com>
2024-07-31 16:02:26 +0500

    create models and tables for time entries

---
+++ app/Models/TimeEntry.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\BreakEntry;
+
+class TimeEntry extends Model
+{
+    use HasFactory;
+
+     protected $fillable = [
+        'user_id',
+        'start_time',
+        'end_time',
+    ];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function breaks()
+    {
+        return $this->hasMany(BreakEntry::class);
+    }
+}
